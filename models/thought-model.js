e9a6c81959e6d6260373ec6ff5dd3be925fd93e1
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-18 13:11:06 -0700

    added code for thought schema in thought model.js

---
+++ models/thought-model.js
@@ -0,0 +1,22 @@
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  });
+  
+  const Thought = mongoose.model('Thought', thoughtSchema);
+  
+  module.exports = Thought;
