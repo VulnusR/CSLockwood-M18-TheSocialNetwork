2ff7fb553adac18f97339d45ac5aea268c92cbfc
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-18 13:08:17 -0700

    created a models folder; added user-model & thought model files. Added code for user model

---
+++ index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const { username, password } = require('./credentials');
+
+mongoose.connect(`mongodb+srv://${username}:${password}@<dbname>.cluster.mongodb.net/<dbname>?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
---
+++ models/user-model.js
@@ -0,0 +1,27 @@
+const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      match: [/.+\@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thought'
+    }],
+    friends: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  });
+  
+  const User = mongoose.model('User', userSchema);
+  
+  module.exports = User;
